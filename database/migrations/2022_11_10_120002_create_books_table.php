5bcf2d5082bb1bbbcb7cc2b08bd4b29ef04b24be
Szabó Erhard Pál <erhard.schneider.1337@gmail.com>
2022-11-22 10:28:58 +0100

    Add Missing Nullable Col Definition

--- database/migrations/2022_11_10_120002_create_books_table.php
+++ database/migrations/2022_11_10_120002_create_books_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('image_link')->nullable();
             $table->foreignId('publisher_id')->constrained()->cascadeOnDelete();
             $table->year('publish_year');
